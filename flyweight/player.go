9421b9132ba1dc4f480231327cc4c3e8c7339426
Gabriel luiz <93672097+GabrielLuizSF@users.noreply.github.com>
2023-04-10 15:41:00 -0300

    update: warning library dependencie repository URL

--- flyweight/player.go
+++ flyweight/player.go
@@ -1,23 +1,23 @@
 package flyweight
 
-import "github.com/GabrielLuizSF/go_url/warning"
+import "github.com/theGOURL/warning"
 
-type Player struct{
-  dress      Dress
-  playerType string
-  lat        int
-  long       int
+type Player struct {
+	dress      Dress
+	playerType string
+	lat        int
+	long       int
 }
 
-func newPlayer(playerType, dressType string) *Player{
-  playerDress, err := getDressFactorySingleInstance().getDressByType(dressType);
-    warning.PRINT_DEFAULT_ERRORS(err,"ERROR: ");
-  return &Player{
-    playerType: playerType,
-    dress:      playerDress,
-  }
+func newPlayer(playerType, dressType string) *Player {
+	playerDress, err := getDressFactorySingleInstance().getDressByType(dressType)
+	warning.PRINT_DEFAULT_ERRORS(err, "ERROR: ")
+	return &Player{
+		playerType: playerType,
+		dress:      playerDress,
+	}
 }
-func (player *Player) newLocation(lat,long int){
-  player.lat  = lat;
-  player.long = long;
+func (player *Player) newLocation(lat, long int) {
+	player.lat = lat
+	player.long = long
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module github.com/GabrielLuizSF/refactoring
 
 go 1.19
 
-require github.com/GabrielLuizSF/go_url v0.0.0-20230107173901-839b381d3a79 // indirect
+require github.com/theGOURL/warning v0.0.0-20230221162525-47579b3f55b9
